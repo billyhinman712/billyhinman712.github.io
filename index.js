ad905c2af36b7cdf1c46c0dfe9b411daafa1f883
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-08-29 13:01:45 -0700

    boilerplate for index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-index.js
+//require stuff
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var express = require('express');
+
+//declare global variables
+var app = express();
+
+//set use statements
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public/'));
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended: false}));
+
+//include controllers/routers
+
+//define routes
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('*', function(req, res){
+	console.log('wildcard route');
+	res.render('error');
+});
+
+//listen to port 3000
+app.listen(3000);
